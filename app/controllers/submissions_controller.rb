aba2a7885c319bf2480466e2b1265e054d0e8791
Scott Dixon <scott@howrefreshing.com.au>
2018-03-30 10:40:15 +1000

    Docker debug

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -28,10 +28,10 @@ class SubmissionsController < ApplicationController
     FileUtils.cp('./public'+@submission.challenge.test_url, 'ruby_docker/tests/'+@submission.challenge.test_identifier)
 
     puts 'Running docker'
-    system('cd ruby_docker && docker build -t ruby-challenge -f Dockerfile.production . && docker build -t ruby-challenge-test -f Dockerfile.test .')
+    system('cd ruby_docker && sudo docker build -t ruby-challenge -f Dockerfile.production . && sudo docker build -t ruby-challenge-test -f Dockerfile.test .')
     puts 'Ran rocker'
 
-    @test_output = `docker run --rm ruby-challenge-test`
+    @test_output = `sudo docker run --rm ruby-challenge-test`
   end
 
   # POST /submissions
